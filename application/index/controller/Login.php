a415ec2acbc5d68b3f7d63766b0cdd505e079188
hmz <1552436066@qq.com>
2018-02-10 16:41:08 +0800

    hmz

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -8,13 +8,23 @@ class Login extends Controller
     public function index()
     {
         if(input('post.')){
+            //设置验证规则
             $validate = new Validate([
                 'name'  => 'require|max:20|min:5',
                 'password' => 'require|min:6|max:18'
             ]);
+            //验证
             if (!$validate->check(input('post.'))) {
-                dump($validate->getError());
+               $error=$validate->getError();
+               foreach(array('name'=>'用户名','password'=>'密码') as $k=>$v){
+                   $error=str_replace($k,$v,$error);
+               }
+                $this->error($error,NULL,'',2);
             }
+            //验证账号
+            //todo::验证账号
+            //渲染主页
+            return view('index',array());
         }else{
            return  view('login');
         }
